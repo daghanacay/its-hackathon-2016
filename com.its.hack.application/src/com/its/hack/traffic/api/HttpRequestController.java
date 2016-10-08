ab86b36c75eb3f746a4ba1bca0a61726b60544c6
developer <developer@5f20af94eba4>
2016-10-08 22:53:34 +0000

    fixinf https problem

--- com.its.hack.application/src/com/its/hack/traffic/api/HttpRequestController.java
+++ com.its.hack.application/src/com/its/hack/traffic/api/HttpRequestController.java
@@ -5,69 +5,94 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ProxySelector;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.security.cert.CertificateException;
+import javax.security.cert.X509Certificate;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 
 public class HttpRequestController {
-	
+
 	private CloseableHttpClient httpclient;
-	
+
 	public HttpRequestController() {
-		httpclient = createHttpClientOrProxy();
+		try {
+			httpclient = createHttpClientOrProxy();
+		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	public HttpResponse getRequest(String url, String responseFormat) throws Exception
-	{
+
+	public HttpResponse getRequest(String url, String responseFormat) throws Exception {
 		return getRequestResponse(url, responseFormat);
-		
+
 	}
-	
+
 	public void close() {
-		try 
-		{
+		try {
 			httpclient.close();
 		} catch (IOException e) {
 			System.out.println("Error closing http client: " + e.getMessage());
 		}
 	}
-	
-	
-	private HttpResponse getRequestResponse(String url, String headerFormat) throws ClientProtocolException, IOException {
+
+	private HttpResponse getRequestResponse(String url, String headerFormat)
+			throws ClientProtocolException, IOException {
 		HttpGet getRequest = new HttpGet(url);
 		getRequest.addHeader("accept", headerFormat);
 		return httpclient.execute(getRequest);
 	}
 
-	private CloseableHttpClient createHttpClientOrProxy() {
-		return HttpClients.custom().setRoutePlanner(new
-     		   SystemDefaultRoutePlanner(ProxySelector.getDefault()))
-     		             .build();
+	private CloseableHttpClient createHttpClientOrProxy()
+			throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+		SSLContextBuilder builder = new SSLContextBuilder();
+		builder.loadTrustMaterial(null, new TrustStrategy() {
+			@Override
+			public boolean isTrusted(java.security.cert.X509Certificate[] arg0, String arg1)
+					throws java.security.cert.CertificateException {
+				return true;
+			}
+		});
+
+		SSLConnectionSocketFactory sslSF = new SSLConnectionSocketFactory(builder.build(),
+				SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+		return HttpClients.custom().setRoutePlanner(new SystemDefaultRoutePlanner(ProxySelector.getDefault()))
+				.setSSLSocketFactory(sslSF).build();
 	}
 
 	public String convertStreamToString(InputStream is) {
 
-	    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-	    StringBuilder sb = new StringBuilder();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		StringBuilder sb = new StringBuilder();
 
-	    String line = null;
-	    try {
-	        while ((line = reader.readLine()) != null) {
-	            sb.append(line + "\n");
-	        }
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    } finally {
-	        try {
-	            is.close();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	    return sb.toString();
+		String line = null;
+		try {
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return sb.toString();
 	}
 }
