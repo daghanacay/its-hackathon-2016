dae73089e6f26cb6108a6c35bdc705d616a0154e
edison728 <edison728@hotmail.com>
2016-10-08 14:51:26 +1100

    Add the mastercard keys

--- com.its.hack.application/src/com/its/hack/mastercard/api/ApiConstants.java
+++ com.its.hack.application/src/com/its/hack/mastercard/api/ApiConstants.java
@@ -2,9 +2,10 @@ package com.its.hack.mastercard.api;
 
 public class ApiConstants {
 	
-	public static final String consumerKey = "";
+	public static final String consumerKey = "0_9siZ0jCU9an62Wb7omXsjNGSE5McR8vkG0FT7G017df549!f380dfc0e8934003913a2351ff8f40190000000000000000";
 	
-	public static final String keyAlias = "";
+	public static final String keyAlias = "its_hackathon_prod";
 	
-	public static final String keyPassword = "";
+	public static final String keyPassword = "priceless";
+	
 }
