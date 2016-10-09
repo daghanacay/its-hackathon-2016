1419c8a4bc01efab28cd6e1ddb4f1d34096b7c20
developer <developer@5f20af94eba4>
2016-10-09 00:00:50 +0000

    starting the destinations

--- com.its.hack.application/src/com/its/hack/application/HackApplication.java
+++ com.its.hack.application/src/com/its/hack/application/HackApplication.java
@@ -1,9 +1,12 @@
 package com.its.hack.application;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.apache.http.HttpResponse;
 import org.json.simple.JSONArray;
@@ -15,6 +18,7 @@ import com.its.hack.converter.MerchantPointOfInterestToMerchantListConverter;
 import com.its.hack.converter.RawBikeStationArrayToBikeStationListConverter;
 import com.its.hack.mastercard.api.locationservices.RequestConfig;
 import com.its.hack.model.BikeStation;
+import com.its.hack.model.Destination;
 import com.its.hack.model.Merchant;
 import com.its.hack.traffic.api.HttpRequestController;
 import com.its.hack.traffic.api.TrafficAPIConstants;
@@ -35,67 +39,80 @@ import osgi.enroute.webserver.capabilities.RequireWebServerExtender;
 @Component(name = "com.its.hack")
 public class HackApplication implements REST {
 
-	private static String CONSUMER_KEY_VALUE 	= "0_9siZ0jCU9an62Wb7omXsjNGSE5McR8vkG0FT7G017df549!f380dfc0e8934003913a2351ff8f40190000000000000000";
-	private static String KEY_ALIAS_VALUE 		= "its_hackathon_prod";
-	private static String KEY_PASSWORD_VALUE 	= "priceless";
-	private static String KEY_PATH_VALUE 		= "resource/its_hackathon_prod_production.p12";
-	
+	private static String CONSUMER_KEY_VALUE = "0_9siZ0jCU9an62Wb7omXsjNGSE5McR8vkG0FT7G017df549!f380dfc0e8934003913a2351ff8f40190000000000000000";
+	private static String KEY_ALIAS_VALUE = "its_hackathon_prod";
+	private static String KEY_PASSWORD_VALUE = "priceless";
+	private static String KEY_PATH_VALUE = "resource/its_hackathon_prod_production.p12";
+
+	private List<BikeStation> bikeStationList = null;
+
 	public String getUpper(String string) {
-	
+
 		return string.toUpperCase();
+
+	}
+
+	public List<Destination> getDestinations(String lat, String lon) {
+		List<Destination> returnVal = new ArrayList<>();
+		Optional<BikeStation> currentStationOpt = this.bikeStationList.stream().filter(
+				bStation -> bStation.latitude.toString().equals(lat) && bStation.longitude.toString().equals(lon))
+				.findFirst();
+		BikeStation currentStation = currentStationOpt.get();
+		if (currentStation.freeBikes > 0) {
+			// we have bikes here hurray
+			returnVal = this.bikeStationList.stream().filter(bStation -> bStation != currentStation).map(bStation -> {
+				Destination dest = new Destination();
+				return dest;
+			}).collect(Collectors.toList());
+		}
 		
+		return returnVal;
 	}
 
+	public List<Merchant> getMerchants(String lat, String lon) throws Exception {
+		RequestConfig config = new RequestConfig(CONSUMER_KEY_VALUE, KEY_ALIAS_VALUE, KEY_PASSWORD_VALUE,
+				KEY_PATH_VALUE);
+		ApiConfig.setAuthentication(config.getAuthentication());
+		ApiConfig.setSandbox(false);
+		RequestMap map = new RequestMap();
+		map.set("radiusSearch", "true");
+		map.set("unit", "km");
+		map.set("pageLength", "50");
+		map.set("distance", "14");
+		map.set("pageOffset", "0");
+		map.set("place.countryCode", "AUS");
+		map.set("place.latitude", lat);
+		map.set("place.longitude", lon);
 
-	public List<Merchant> getMerchants(String lat, String lon) throws Exception
-	{
-		RequestConfig config = new RequestConfig(CONSUMER_KEY_VALUE, KEY_ALIAS_VALUE, KEY_PASSWORD_VALUE, KEY_PATH_VALUE);
-        ApiConfig.setAuthentication(config.getAuthentication());
-        ApiConfig.setSandbox(false);
-        RequestMap map = new RequestMap();
-        map.set("radiusSearch", "true");
-        map.set("unit", "km");
-        map.set("pageLength", "50");
-        map.set("distance", "14");
-        map.set("pageOffset", "0");
-        map.set("place.countryCode", "AUS");
-        map.set("place.latitude", lat);
-        map.set("place.longitude", lon);
+		MerchantPointOfInterest response = MerchantPointOfInterest.create(map);
 
-        MerchantPointOfInterest response = MerchantPointOfInterest.create(map);
+		List<Merchant> merchantList = MerchantPointOfInterestToMerchantListConverter
+				.convertMerchantPointOfInterestToMerchantList(response);
 
-        List<Merchant> merchantList = MerchantPointOfInterestToMerchantListConverter.convertMerchantPointOfInterestToMerchantList(response);
-
-        return merchantList;
+		return merchantList;
 	}
-	
-	public List<BikeStation> getBikeStations() 
-	{
+
+	public List<BikeStation> getBikeStations() {
 		HttpRequestController controller = new HttpRequestController();
-		List<BikeStation> bikeStationList = null;
-		try 
-		{
+		try {
 			HttpResponse response = controller.getRequest(TrafficAPIConstants.URL, TrafficAPIConstants.JSON_HEADER);
-						
-			if ((response!= null) &&  response.getStatusLine().getStatusCode() != 200) {
-				throw new RuntimeException("Failed : HTTP error code : "
-				   + response.getStatusLine().getStatusCode());
+
+			if ((response != null) && response.getStatusLine().getStatusCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
 			}
 
 			InputStream input = response.getEntity().getContent();
 			String responseResult = controller.convertStreamToString(input);
 			JSONParser parser = new JSONParser();
-			JSONObject json = (JSONObject)parser.parse(responseResult);
-			JSONObject networkJson = (JSONObject)json.get(TrafficAPIConstants.NETWORK_KEY);
-			JSONArray stationJsonArray = (JSONArray)networkJson.get(TrafficAPIConstants.STATIONS_KEY);
+			JSONObject json = (JSONObject) parser.parse(responseResult);
+			JSONObject networkJson = (JSONObject) json.get(TrafficAPIConstants.NETWORK_KEY);
+			JSONArray stationJsonArray = (JSONArray) networkJson.get(TrafficAPIConstants.STATIONS_KEY);
 
-			bikeStationList = RawBikeStationArrayToBikeStationListConverter.convertRawBikeStationArrayToBikeStationList(stationJsonArray);
-		}
-		catch (Exception ex)
-		{
+			bikeStationList = RawBikeStationArrayToBikeStationListConverter
+					.convertRawBikeStationArrayToBikeStationList(stationJsonArray);
+		} catch (Exception ex) {
 			System.out.println("GET bike stations failed: " + ex.getMessage());
-		}
-		finally {
+		} finally {
 			controller.close();
 		}
 		return bikeStationList;
